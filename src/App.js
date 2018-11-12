1b60f89866f3b62b00c228eba08db1a3f27bb91f
Edwin Hsia <hsiae@pragmatics.com>
2018-11-12 13:51:56 -0500

    Revert "Added page transitions"
    This reverts commit 88f14201e8a3943cde7930916f03a0bf61ae9121.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import { Button } from 'reactstrap';
+// import PageTransition from 'react-router-page-transition';
 
 //Pages
 import Main from './pages/Main/Main';
@@ -10,33 +11,25 @@ import NoMatch from './pages/404Page/NoMatch';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Resume from './pages/Resume/Resume';
 
-class App extends Component {
 
+class App extends Component {
   render() {
     return (
       <div className="main-body">
-
-        <Router>
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={{ enter: 300, exit: 300}}
-              classNames={'fade'}
-            >
-              <Switch location={location}>
-                <Route exact path="/" component={Main} />
-                <Route exact path="/aboutme" component={Aboutme} />
-                <Route exact path="/portfolio" component={Portfolio} />
-                <Route exact path="/resume" component={Resume} />
-                <Route component={NoMatch} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        </Router>
-
+        {/* <PageTransition> */}
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/aboutme" component={Aboutme} />
+              <Route exact path="/portfolio" component={Portfolio} />
+              <Route exact path="/resume" component={Resume} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        {/* </PageTransition> */}
       </div>
     );
   }
 }
 
-export default withRouter(App);
+export default App;
--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -1,36 +1,35 @@
 import React from "react";
 import './NavButton.css';
-import { Link } from "react-router-dom";
 
 const NavButton = props => (
     <div className="row" id="button-container">
 
         <div id="home-button">
-            <Link to="/" id="home-text">
+            <a href="/" id="home-text">
                 <i className="material-icons">home</i>
                 <div>Home</div>
-                </Link>
+            </a>
         </div>
 
         <div id="aboutme-button">
-            <Link to="/aboutme" id="aboutme-text">
+            <a href="/aboutme" id="aboutme-text">
                 <i className="material-icons">person</i>
                 <div>About Me</div>
-            </Link>
+            </a>
         </div>
 
         <div id="portfolio-button">
-            <Link to="/portfolio" id="portfolio-text">
+            <a href="/portfolio" id="portfolio-text">
                 <i className="material-icons">work</i>
                 <div>Portfolio</div>
-            </Link>
+            </a>
         </div>
 
         <div id="resume-button">
-            <Link to="/resume" id="resume-text">
+            <a href="/resume" id="resume-text">
                 <i className="material-icons">library_books</i>
                 <div>Resume</div>
-            </Link>
+            </a>
         </div>
 
     </div>
